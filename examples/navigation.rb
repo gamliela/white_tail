84e9703616c4d09d9f3e41dedbfacaa38b2d5c18
Alon Gamliel <alon@gamliela.com>
2019-06-10 19:09:38 +1200

    Comment out unavailable commands/dsl

--- examples/navigation.rb
+++ examples/navigation.rb
@@ -3,32 +3,32 @@ require "json"
 require "white_tail"
 
 class SearchResultPage < WhiteTail::DSL::Nodes::Page
-  text :contents, "//div[@id='toc']"
+  # text :contents, "//div[@id='toc']"
 end
 
 class SearchResultsPage < WhiteTail::DSL::Nodes::Page
-  text :results_count, ".//div[@class='results-info']"
-  within ".//div[@class='searchresults mw-searchresults-has-iw']" do
-    sections :results, ".//li[@class='mw-search-result']" do
-      text :num_words, ".//div[@class='mw-search-result-data']"
-      section :heading, ".//div[@class='mw-search-result-heading']/a" do
-        text :title, nil
-        open :result_link, nil, page_class => SearchResultPage
-      end
-    end
-    load_more :results, ".//a[@class='mw-nextlink']"
-  end
+  # text :results_count, ".//div[@class='results-info']"
+  # within ".//div[@class='searchresults mw-searchresults-has-iw']" do
+  #   sections :results, ".//li[@class='mw-search-result']" do
+  #     text :num_words, ".//div[@class='mw-search-result-data']"
+  #     section :heading, ".//div[@class='mw-search-result-heading']/a" do
+  #       text :title, nil
+  #       open :result_link, nil, page_class => SearchResultPage
+  #     end
+  #   end
+  #   load_more :results, ".//a[@class='mw-nextlink']"
+  # end
 end
 
 class SearchPage < WhiteTail::DSL::Nodes::Page
-  section :search_form, ".//form[@id='search']" do
-    type :query_string, ".//input[@name='search']", "spiders"
-    click :submit, ".//input[@type='submit']", :page_class => SearchResultsPage
-  end
+  # section :search_form, ".//form[@id='search']" do
+  #   type :query_string, ".//input[@name='search']", "spiders"
+  #   click :submit, ".//input[@type='submit']", :page_class => SearchResultsPage
+  # end
 end
 
 class HomePage < WhiteTail::DSL::Nodes::Page
-  click :search_button, ".//form[@id='searchform']//input[@type='submit']", :page_class => SearchPage
+  # click :search_button, ".//form[@id='searchform']//input[@type='submit']", :page_class => SearchPage
 end
 
 WhiteTail.project :spiders_wiki do
