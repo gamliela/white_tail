2714657e5311bee4951c1cef3deee16d2d090a60
Alon Gamliel <alon@gamliela.com>
2019-06-18 23:38:06 +1200

    Made url optional in "visit" command, close the window when it finishes

--- lib/white_tail/dsl/commands/visit.rb
+++ lib/white_tail/dsl/commands/visit.rb
@@ -4,15 +4,23 @@ module WhiteTail
       class Visit < Base
         prepend ScriptExecuter
 
-        REQUIRED_OPTIONS = [:node_class, :url]
-        ALLOWED_OPTIONS = []
+        REQUIRED_OPTIONS = [:node_class]
+        ALLOWED_OPTIONS = [:url]
 
         def execute(execution_context)
-          execution_context.session.visit(options[:url])
-          page_node = options[:node_class].new
-          page_execution_context = ExecutionContext.new(execution_context.session, page_node)
-          Helpers.execute_script(script, page_execution_context)
-          page_node
+          if options[:url]
+            page_node = options[:node_class].new
+            session = execution_context.session
+            session.within_window(session.open_new_window) do
+              session.visit(options[:url])
+              page_execution_context = ExecutionContext.new(session, page_node)
+              Helpers.execute_script(script, page_execution_context)
+              session.current_window.close
+            end
+            page_node
+          else
+            Nodes::Field.new(nil)
+          end
         end
       end
     end
